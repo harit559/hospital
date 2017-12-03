a7db29fce8bd982000958a03d4c8a5438e79ee7a
Charles Whitesman <life.particles@gmail.com>
2017-12-03 16:08:50 +0700

    ben's

--- src/com/company/User.java
+++ src/com/company/User.java
@@ -1,10 +1,14 @@
 package com.company;
 
-public class User {
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public abstract class User {
     protected String name;
     protected String surname;
     protected String user;
     protected String password;
+    protected boolean isDoctor;
 
     public User(String name, String surname, String user, String password) {
         this.name = name;
@@ -13,11 +17,19 @@ public class User {
         this.password = password;
     }
 
-    public boolean verify_login(String user, String password) {
-        if((this.user == user) && (this.password == password)) {
+    public boolean verify_login( String password) {
+        if(this.password == password) {
             return true;
         }
         return false;
+    }
+
+    public boolean isDoctor() {
+        return isDoctor;
+    }
+
+    private String getUser() {
+        return user;
     }
 
     public String getName() {
@@ -26,6 +38,23 @@ public class User {
 
     public String getSurname() {
         return surname;
+    }
+
+    public static User findByUsername(ArrayList users, String username){
+        Iterator userIterator = users.iterator();
+
+        while(userIterator.hasNext()) {
+            User each = (User) userIterator.next();
+            String test = each.getUser();
+            if(test == username) {
+                return each;
+            }
+            else {
+                System.out.println("it's not him.");
+            }
+        }
+
+        return null;
     }
 
 }
